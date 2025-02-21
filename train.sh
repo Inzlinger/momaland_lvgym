0a708f1c93cfc442491298384d325b89caca87b9
Jakob Ohm <jakob.ohm@protonmail.com>
2025-02-21 10:23:46 +0100

    train script

---
+++ train.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --partition=normal
+#SBATCH --time=72:00:00
+#SBATCH --output=slurm/slurm-exec_train-%j.out
+#SBATCH --error=slurm/slurm-exec_train-%j.err
+#SBATCH --job-name=rl-train-%j
+#SBATCH --gres=gpu:full:1
+
+# Enter your environment path here
+PYTHON_ENV="../moma_env"
+export WANDB_API_KEY=
+source $PYTHON_ENV/bin/activate
+python3.9 .\momaland\learning\cooperative_momappo\continuous_momappo.py --envi-id moegridLVFeeder_v0 --exp-name first_test --seed 42 --ref-point 1 1 1 1 1 0 --track --timesteps-per-weight 1000
